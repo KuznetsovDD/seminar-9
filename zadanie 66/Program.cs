ec36bf81dea157d1526d5e3a35806e9ee4e0afd5
Atlant <cuz.den30@gmail.com>
2022-12-16 17:24:51 +0300

    zadanie 66

--- zadanie 66/Program.cs
+++ zadanie 66/Program.cs
@@ -4,3 +4,13 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int GetSum(int num1, int num2)
+{
+    if(num1==num2)
+    {
+        return num2;
+    }
+    return(num1 + GetSum(num1+1, num2));
+}
+
+System.Console.WriteLine(GetSum(1, 3));
