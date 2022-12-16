4f1bd6715cf41f1afb8dfcb6b4f7be8fde349281
Atlant <cuz.den30@gmail.com>
2022-12-16 18:06:20 +0300

    zadanie 68

--- zadanie 68/Program.cs
+++ zadanie 68/Program.cs
@@ -4,3 +4,17 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
+int Akkerman(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2 + 1;
+    }
+    if (num2 == 0)
+    {
+        return Akkerman(num1 - 1, 1);
+    }
+    return Akkerman(num1 - 1, Akkerman(num1, num2 - 1));
+}
+
+System.Console.WriteLine(Akkerman(3, 2));
